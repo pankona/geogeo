f11b7d4e2381217075398a886b5c04dcf66305bd
pankona <yosuke.akatsuka@gmail.com>
2017-10-02 21:18:47 +0900

    remove unnecessary file and fix indentation and remove run

--- Makefile
+++ Makefile
@@ -5,4 +5,4 @@ build:
 	@npm run build --scripts-prepend-node-path
 
 run: build
-	@npm run start --scripts-prepend-node-path
+	@npm start --scripts-prepend-node-path
--- public/main.js
+++ public/main.js
@@ -7,31 +7,31 @@ const url = require('url');
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 800, height: 600});
 
-    mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
-    mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
-    mainWindow.on('closed', function () {
-        mainWindow = null
-    })
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
 }
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 });
 
-app.on('activate', function () {
-    if (mainWindow === null) {
-        createWindow()
-    }
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
 });
