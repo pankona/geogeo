97673210f7cfae1f7b2e20e2dae45bb844d337c7
pankona <yosuke.akatsuka@gmail.com>
2017-10-05 21:30:54 +0900

    show pokelist

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import PokeList from './PokeList';
 import './styles/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
-        hello react world!
+        <PokeList />
       </div>
     );
   }
-}
+};
 
 export default App;
