34ad5e481486fc6d5a8f50b2880e9404274d6b02
pankona <yosuke.akatsuka@gmail.com>
2017-10-06 22:58:45 +0900

    add missing files

---
+++ src/components/PokeCell.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import sprites from '../assets/sprites.png';
+import './styles/PokeCell.css';
+
+const PokeCell = ( {pokeClass} ) => {
+  const { id, backgroundPosition } = pokeClass;
+  const style = {
+    backgroundImage: `url(${sprites})`,
+    backgroundPosition
+  };
+  return <button style={style} className="poke-cell"></button>
+};
+
+export default PokeCell;
---
+++ src/components/PokeList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PokeCell from './PokeCell';
+import { pokeClasses } from '../pokeClasses';
+import './styles/PokeList.css';
+
+const PokeList = () => {
+  const cells = pokeClasses.map(pokeClass => {
+    return (
+      <PokeCell
+        key={pokeClass.id}
+        pokeClass={pokeClass}
+      />
+    );
+  });
+  return (
+    <section className="poke-list">
+      {cells}
+    </section>
+  );
+};
+
+export default PokeList;
